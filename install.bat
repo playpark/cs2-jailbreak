0f736064315f3bd6ddb26a648ae222b94f41f8a8
exkludera <51145038+exkludera@users.noreply.github.com>
2024-08-30 22:26:17 +0200

    clean up

--- install.bat
+++
@@ -1,2 +0,0 @@
-cp src/bin/Debug/net8.0/* "../../plugins/Cs2Jailbreak/"
-cp -r lang "../../plugins/Cs2Jailbreak/"
--- install.sh
+++
@@ -1,3 +0,0 @@
-yes | cp src/bin/Debug/net8.0/* "../../plugins/Cs2Jailbreak/"
-#yes | cp WardenService/bin/Debug/net8.0/* "../../shared/WardenService/"
-cp -r lang "../../plugins/Cs2Jailbreak/"
